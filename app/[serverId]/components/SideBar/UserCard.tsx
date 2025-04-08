13836dd94e5cd759134966fc4568d05a6387c7a3
fabiconcept <favourajokubi@gmail.com>
2025-04-08 13:28:10 +0100

    make Usercard a standalone component

--- app/[serverId]/components/SideBar/UserCard.tsx
+++ app/[serverId]/components/SideBar/UserCard.tsx
@@ -1,5 +1,28 @@
-export default function UserCard() {
+import { formatDistanceToNow } from 'date-fns';
+
+interface UserCardProps {
+    username: string;
+    isOnline: boolean;
+    lastSeen: Date;
+}
+
+export default function UserCard({ username, isOnline, lastSeen }: UserCardProps) {
     return (
-        <div>UserCard</div>
-    )
+    <div className="flex items-center gap-3 px-4 py-1 hover:bg-white/2 transition-colors cursor-pointer">
+        <div className="relative">
+            <div className="w-14 h-14 rounded-full bg-white/10 flex items-center justify-center">
+                {username[0].toUpperCase()}
+            </div>
+            <div className={`absolute bottom-1 right-1 w-3 h-3 rounded-full border-2 border-black/20 ${isOnline ? 'bg-green-500' : 'bg-gray-500'}`} />
+        </div>
+        <div className="flex-1">
+            <h3 className="text-white font-medium">{username}</h3>
+            {!isOnline && (
+                <p className="text-xs text-gray-400">
+                    Last seen {formatDistanceToNow(new Date(lastSeen))} ago
+                </p>
+            )}
+        </div>
+    </div>
+    );
 }
--- app/[serverId]/components/SideBar/index.tsx
+++ app/[serverId]/components/SideBar/index.tsx
@@ -1,33 +1,10 @@
 "use client";
 
 import { useServerStore } from "@/store/useServerStore";
-import { formatDistanceToNow } from 'date-fns';
 import Link from "next/link";
+import UserCard from "./UserCard";
 
-interface UserCardProps {
-    username: string;
-    isOnline: boolean;
-    lastSeen: Date;
-}
 
-const UserCard = ({ username, isOnline, lastSeen }: UserCardProps) => (
-    <div className="flex items-center gap-3 px-4 py-1 hover:bg-white/2 transition-colors cursor-pointer">
-        <div className="relative">
-            <div className="w-14 h-14 rounded-full bg-white/10 flex items-center justify-center">
-                {username[0].toUpperCase()}
-            </div>
-            <div className={`absolute bottom-1 right-1 w-3 h-3 rounded-full border-2 border-black/20 ${isOnline ? 'bg-green-500' : 'bg-gray-500'}`} />
-        </div>
-        <div className="flex-1">
-            <h3 className="text-white font-medium">{username}</h3>
-            {!isOnline && (
-                <p className="text-xs text-gray-400">
-                    Last seen {formatDistanceToNow(new Date(lastSeen))} ago
-                </p>
-            )}
-        </div>
-    </div>
-);
 
 export default function SideBar() {
     const { server, activeUsers, isOwner, } = useServerStore();
