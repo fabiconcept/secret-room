78fb0dbc5b77671854a791628da074bb944b6cbe
fabiconcept <favourajokubi@gmail.com>
2025-04-08 21:27:40 +0100

    Implement invite global and unique

--- app/[serverId]/components/SideBar/index.tsx
+++ app/[serverId]/components/SideBar/index.tsx
@@ -1,16 +1,40 @@
 "use client";
 
 import { useServerStore } from "@/store/useServerStore";
-import Link from "next/link";
 import UserCard from "./UserCard";
-
+import { performCopy } from "@/utils";
+import { apiService } from "@/utils/services/api.service";
+import { getItem } from "@/utils/localStorage";
+import { Auth } from "@/app/types/index.type";
+import { appURL } from "@/constants/index.constant";
 
 
 export default function SideBar() {
     const { server, activeUsers, isOwner, } = useServerStore();
 
+    
     if (!server) return null;
+    
+    const generateUniqueInvitation = async () => {
+        const authFallback = {
+            userId: '',
+            username: '',
+            token: ''
+        };
+        const auth = getItem<Auth>(server.server_id, authFallback);
+        try {
+            const response = await apiService.generateUniqueInvitation(server.server_id, auth.token);
+            console.log({
+                response
+            })
 
+            if (response.data) {
+                performCopy(`${appURL}/uinvite/${response.data.inviteCode}`);
+            }
+        } catch (error) {
+            console.error('Error generating unique invitation:', error);
+        }
+    };
     return (
         <section className="flex flex-col w-[clamp(20rem,100%,25rem)] h-full bg-black/20 border-r border-b border-white/10">
             <header className={"w-full p-10 text-center text-white border-b border-gray-500/20 text-xl font-semibold bg-black/5 backdrop-blur-[2px]"}>
@@ -43,11 +67,18 @@ export default function SideBar() {
                 </div>
             </div>
             {isOwner && <footer className="w-full p-5 text-center text-white flex items-center gap-3 border-b border-gray-500/20 bg-black/5 backdrop-blur-[2px]">
-                {/* <Link href={`/ginvite/${server?.global_invitation_id}`}>{server?.global_invitation_id}</Link> */}
-                <div className="flex border border-white/5 hover:border-white/20 cursor-pointer active:scale-95 text-center items-center justify-center gap-2 p-3 rounded-3xl bg-white/10 flex-1">
+                <div 
+                    className="flex border border-white/5 hover:border-white/20 cursor-pointer active:scale-95 text-center items-center justify-center gap-2 p-3 rounded-3xl bg-white/10 flex-1"
+                    title="Copy Global Invite"
+                    onClick={() => performCopy(`${appURL}/ginvite/${server?.global_invitation_id}` || '')}
+                >
                     Global Invite
                 </div>
-                <div className="flex border border-white/5 hover:border-white/20 cursor-pointer active:scale-95 text-center items-center justify-center gap-2 p-3 rounded-3xl bg-white/10 flex-1">
+                <div 
+                    className="flex border border-white/5 hover:border-white/20 cursor-pointer active:scale-95 text-center items-center justify-center gap-2 p-3 rounded-3xl bg-white/10 flex-1"
+                    title="Generate Unique Invite"
+                    onClick={generateUniqueInvitation}
+                >
                     Unique Invite
                 </div>
             </footer>}
