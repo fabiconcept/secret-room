63489de8fcd4ae6d6ff99148d02a4b951a14fb2f
fabiconcept <favourajokubi@gmail.com>
2025-04-17 09:20:47 +0100

    Control matrix grid background with useAppStore

--- store/useAppStore.ts
+++ store/useAppStore.ts
@@ -2,10 +2,20 @@ import { create } from "zustand";
 
 interface AppStore {
     sideBarExpanded: boolean;
+    homeBackgroundFontSize: number;
+    homeTextColor: string;
     toggleSideBar: () => void;
+    setSideBarExpanded: (expanded: boolean) => void;
+    setHomeBackgroundFontSize: (fontSize: number) => void;
+    setHomeTextColor: (color: string) => void;
 }
 
 export const useAppStore = create<AppStore>((set)=>({
     sideBarExpanded: true,
-    toggleSideBar: () => set((state) => ({ sideBarExpanded: !state.sideBarExpanded }))
+    homeBackgroundFontSize: 16,
+    homeTextColor: '#000',
+    toggleSideBar: () => set((state) => ({ sideBarExpanded: !state.sideBarExpanded })),
+    setSideBarExpanded: (expanded) => set({ sideBarExpanded: expanded }),
+    setHomeBackgroundFontSize: (fontSize) => set({ homeBackgroundFontSize: fontSize }),
+    setHomeTextColor: (color) => set({ homeTextColor: color })
 }))
